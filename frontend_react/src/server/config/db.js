768c5063a65cc1c16be7d844005e830f29f331f7
MarcArcherCiscar <33457293+MarcArcherCiscar@users.noreply.github.com>
2022-07-06 22:29:09 +0200

    Delete frontend_react/src/server/config directory

--- frontend_react/src/server/config/db.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql')
-const db = mysql.createConnection({
-host: "10.0.47.217:3307",
-user: "plinder",
-password: "dR1]d5C_Zb",
-database:"plinder" 
-})
-
-module.exports = db;
